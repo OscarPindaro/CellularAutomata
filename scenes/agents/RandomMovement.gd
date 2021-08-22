31b743fb7e0e4a85df09850197c858b5018c94bf
Oscar Pindaro <oscar.pindaro@gmail.com>
2021-08-22 19:42:32 +0200

    added some signals and drawing directions

--- scenes/agents/RandomMovement.gd
+++ scenes/agents/RandomMovement.gd
@@ -5,14 +5,29 @@ var speed = 100
 var currVelocity = Vector2(1,0) *speed
 # var b = "text"
 var rng = RandomNumberGenerator.new()
+var arrowSprite : Sprite
+
+signal collisionSignal
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	arrowSprite = get_node("arrow")
+	connect("collisionSignal", get_parent(), "assignReward")
+
 
 func _process(delta):
 	if rng.randf_range(0, 1)<  0.05:
-		var degrees = rng.randf_range(0, PI/4)
+		var degrees = rng.randf_range(-PI/4, PI/4)
 		currVelocity = currVelocity.normalized().rotated(degrees)*speed
-	currVelocity = move_and_slide(currVelocity)
+	move_and_slide(currVelocity)
+	allineSpriteToVector(currVelocity)
+	if(get_slide_count() > 0):
+		emit_signal("collisionSignal")
+	
+
+func allineSpriteToVector(vector : Vector2):
+	arrowSprite.rotation_degrees =rad2deg(currVelocity.angle()+PI/2)
+	
+	
+	
 
