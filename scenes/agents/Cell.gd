ef949f5244168c22ddff31552d5ee39701454723
Oscar Pindaro <oscar.pindaro@gmail.com>
2021-08-24 14:47:02 +0200

    added code that computes raycasting

--- scenes/agents/Cell.gd
+++ scenes/agents/Cell.gd
@@ -1,7 +1,9 @@
 extends KinematicBody2D
 
-var speed = 100
-var currVelocity = Vector2(1,0) *speed
+const SPEED = 100
+const OMEGA = 5 #rad/s
+var currVelocity = Vector2(1,0) *SPEED
+var currOmega = 0;
 var rng = RandomNumberGenerator.new()
 var arrowSprite : Sprite
 
@@ -13,9 +15,25 @@ func _ready():
 
 
 func _process(delta):
-	if rng.randf_range(0, 1)<  0.05:
-		var degrees = rng.randf_range(-PI/4, PI/4)
-		currVelocity = currVelocity.normalized().rotated(degrees)*speed
+	var spaceState = get_world_2d().direct_space_state
+	var velDirVector = currVelocity.normalized()*32*4
+	var direction = Vector2(velDirVector.x, velDirVector.y);
+	var result = spaceState.intersect_ray(position, position+direction, [self], collision_mask)
+	if result:
+		if (typeof(result.collider) == TYPE_OBJECT):
+			print(result.collider.get_class())
+			print(result.collider.name)
+	
+	var realization = rng.randf_range(0, 1);
+	if realization <  0.1:
+		currOmega = -OMEGA
+	elif realization >  0.7:
+		currOmega = OMEGA
+	else:
+		currOmega = 0
+		
+	
+	currVelocity = currVelocity.normalized().rotated(currOmega*delta)*SPEED
 	move_and_slide(currVelocity)
 	allineSpriteToVector(currVelocity)
 	if(get_slide_count() > 0):
